8af7d30ff4e90f76e440bd48830094a728f1e6c5
Yonas44 <tesfu.yonas1350@gmail.com>
2022-12-23 00:12:21 +0300

    Update projects

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import './style.css';
 import 'bootstrap';
 import Profile from '../assets/images/profile-pic2.png';
-import Project1 from '../assets/images/todo.png';
-import Project2 from '../assets/images/awesome_books.png';
-import Project3 from '../assets/images/artex.png';
-import Project4 from '../assets/images/moviex.png';
+import Project1 from '../assets/images/moviex.png';
+import Project2 from '../assets/images/todo.png';
+import Project3 from '../assets/images/bookstore.png';
 import { displayProjects } from './modules/displayProjects';
 import { displayMenu } from './modules/displayMenu';
 
@@ -13,7 +12,7 @@ const myProfilePic = new Image();
 myProfilePic.src = Profile;
 introPicture.appendChild(myProfilePic);
 const listOfProjects = document.querySelector('.projects-list');
-const projects = [Project1, Project2, Project3, Project4];
+const projects = [Project1, Project2, Project3];
 displayProjects(listOfProjects, projects);
 
 const navMenuOpen = document.querySelector('.nav-menu-open');
--- src/modules/card.js
+++ src/modules/card.js
@@ -1,5 +1,14 @@
 export const cardContainer = [
   {
+    title: 'MovieX',
+    languages: ['HTML', 'CSS', 'JavaScript'],
+    text: 'This is a movie application that lets users choose between different movies to watch based on the number of likes and comments. Built with ES6 and webpack',
+    Links: [
+      'https://github.com/Abdessittir/JavaScript-capstone',
+      'https://abdessittir.github.io/JavaScript-capstone/dist/',
+    ],
+  },
+  {
     title: 'To-Do_APP',
     languages: ['JavaScript', 'Bootstrap', 'CSS'],
     text: 'A todo-list application which can be used to keep track of daily tasks. Users can add, edit and remove tasks from their list easily. Built with JavaScript.',
@@ -9,27 +18,12 @@ export const cardContainer = [
     ],
   },
   {
-    title: 'Awesome_Books',
-    languages: ['HTML', 'CSS', 'JavaScript'],
-    text: 'An application built to create a list of books which can be added and removed as needed, built with HTML, CSS and JavaScript using SPA concept.',
-    Links: ['https://github.com/yonas44/Awesome-books_ES6', '#'],
-  },
-  {
-    title: 'Artex-conference-website',
-    languages: ['JavaScript', 'Bootstrap', 'CSS'],
-    text: 'A conference website to display event program, featured speakers along with date of the event. Built with HTML, CSS and JavaScript.',
+    title: 'Bookstore_App',
+    languages: ['React', 'Redux'],
+    text: 'An app that allows users to add and remove books, built with React and Redux following SPA concept.',
     Links: [
-      'https://github.com/yonas44/Artex-conference-website',
-      'https://yonas44.github.io/Artex-conference-website/',
-    ],
-  },
-  {
-    title: 'MovieX',
-    languages: ['HTML', 'CSS', 'JavaScript'],
-    text: 'This is a movie application that lets users choose between different movies to watch based on the number of likes and comments. Built with ES6 and webpack',
-    Links: [
-      'https://github.com/Abdessittir/JavaScript-capstone',
-      'https://abdessittir.github.io/JavaScript-capstone/dist/',
+      'https://github.com/yonas44/bookstore-app',
+      'https://react-book-store.onrender.com/',
     ],
   },
 ];
